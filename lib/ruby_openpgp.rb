1536c9ef3d51c39331508fbbf3695c62be509d33
rudietuesdays <rae@throneless.tech>
2021-04-06 12:01:51 -0400

    initial commit

---
+++ lib/ruby_openpgp.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ruby_openpgp/version"
+
+module RubyOpenpgp
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/ruby_openpgp_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RubyOpenpgp do
+  it "has a version number" do
+    expect(RubyOpenpgp::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
